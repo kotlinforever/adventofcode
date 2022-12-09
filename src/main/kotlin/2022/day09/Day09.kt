4a4a40b756d2d8c763efa2b908d89a3a211ad8ed
admin <!ZeusSonOfCronus!>
2022-12-09 15:11:23 +0100

    day 9

--- src/main/kotlin/2022/day09/Day09.kt
+++ src/main/kotlin/2022/day09/Day09.kt
@@ -24,59 +24,80 @@ class Day09 {
 //        part1("src/main/kotlin/2022/day09/input.txt")
 
 //        part2(FileHandler().getLinesAsString(File("src/main/kotlin/2022/day09/input")), 2) // 13
-//        part2(FileHandler().getLinesAsString(File("src/main/kotlin/2022/day09/test-part2.txt")), 10) // 36
-        part2(FileHandler().getLinesAsString(File("src/main/kotlin/2022/day09/input2.txt")), 10)
+        part2(FileHandler().getLinesAsString(File("src/main/kotlin/2022/day09/test-part2.txt")), 10) // 36
+//        part2(FileHandler().getLinesAsString(File("src/main/kotlin/2022/day09/input2.txt")), 10)
     }
 
     private fun part2(lines: MutableList<String>, noOfSnakes: Int) {
 
-        for (i in 0 until noOfSnakes ) {
+        for (i in 0 until noOfSnakes) {
             listPart2.add(SnakePart("Snake-$i"))
         }
 
         for (line in lines) {
-            println(line)
+//            println(line)
             val direction = line.split(" ")[0]
             val steps = line.split(" ")[1].toInt()
             for (i in 0 until steps) {
+//                print(".")
                 move(direction, listPart2[0])
-                for (j in 0 until noOfSnakes-1) {
+//                printSnake(listPart2[0])
+                for (j in 0 until noOfSnakes - 1) {
                     moveAfter(listPart2[j], listPart2[j + 1])
                 }
             }
+            println("\n")
         }
 
         for (snake in listPart2) {
             println(snake)
         }
-        println("\nsteps...: ${listPart2[noOfSnakes-1].steps.size}")
+        println("\nsteps...: ${listPart2[noOfSnakes - 1].steps.size}")
     }
 
-    private fun print() {
-        for (snake in listPart2) {
-            for (i in 0..10) {
-                print(i)
+    private fun printSnake(snakePart: SnakePart) {
+        println()
+        for (i in 0..10) {
+            for (j in 0..10) {
+                if (snakePart.x == i && snakePart.y == j) {
+                    print("X")
+                } else {
+                    print(".")
+                }
             }
-            println(snake.name)
+            println()
         }
+        println("*************************************************************")
     }
 
     private fun moveAfter(head: SnakePart, tail: SnakePart) {
         if (head.x - 2 == tail.x) {
             tail.x = tail.x + 1
-            tail.y = head.y
+            if (tail.y > head.y)
+                tail.y = tail.y - 1
+            else if (tail.y < head.y)
+                tail.y = tail.y + 1
         }
         if (head.x + 2 == tail.x) {
             tail.x = tail.x - 1
-            tail.y = head.y
+            if (tail.y > head.y)
+                tail.y = tail.y - 1
+            else if (tail.y < head.y)
+                tail.y = tail.y + 1
         }
         if (head.y - 2 == tail.y) {
             tail.y = tail.y + 1
-            tail.x = head.x
+            if (tail.x > head.x)
+                tail.x = tail.x - 1
+            else if (tail.y < head.y)
+                tail.x = tail.x + 1
         }
         if (head.y + 2 == tail.y) {
             tail.y = tail.y - 1
-            tail.x = head.x
+            if (tail.x > head.x)
+                tail.x = tail.x - 1
+            else if (tail.y < head.y)
+                tail.x = tail.x + 1
         }
         tail.steps = tail.steps.plus(Pair(tail.x, tail.y))
     }
